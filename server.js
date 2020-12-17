1a002e33eafd499c545a4e24ab8570b853031631
FrancaTM <francatm@outlook.com>
2020-12-17 19:24:13 -0300

    initialize sequelize

--- server.js
+++ server.js
@@ -16,6 +16,32 @@ app.use(bodyParser.json());
 // parse requests of content-type application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const db = require("./app/models");
+const Role = db.role;
+
+// db.sequelize.sync();
+db.sequelize.sync({ force: true }).then(() => {
+  console.log("drop and resync db");
+  initial();
+});
+
+function initial() {
+  Role.create({
+    id: 1,
+    name: "user",
+  });
+
+  Role.create({
+    id: 2,
+    name: "moderator",
+  });
+
+  Role.create({
+    id: 3,
+    name: "admin",
+  });
+}
+
 // simple route
 app.get("/", (req, resp) => {
   resp.json({ message: "welcome to sample node jwt auth application" });
